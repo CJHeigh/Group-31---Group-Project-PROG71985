2d755af58de3e21d402da0f5755f573fa0065396
Garrett Todd <garrettptodd@outlook.com>
2021-12-10 22:00:08 -0500

    Small bug fix

--- Program/Wallet.c
+++ Program/Wallet.c
@@ -26,7 +26,6 @@ void DeleteWallet(Wallet walletToDelete)
 	*walletToDelete.walletUsername = '\0'; // Changed from NULL to '\0' to remove warning C4047
 	*walletToDelete.walletPassword = '\0'; // Same as line above
 	walletToDelete.btcAmount = 0;
-
 }
 
 Wallet UpdateWallet(Wallet walletToUpdate, char usernameToUpdateTo[BUFFER_SIZE], char passwordToUpdateTo[BUFFER_SIZE], float btcToUpdateTo)
